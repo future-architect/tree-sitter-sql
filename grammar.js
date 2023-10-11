e5cdcc4809f319b0b58d005451537c93e8a89a38
yamada2915 <s.yamada.bg@future.co.jp>
2023-10-11 14:36:09 +0900

    fix null type_cast

--- grammar.js
+++ grammar.js
@@ -543,6 +543,9 @@ module.exports = grammar({
         $.number,
         $.identifier,
         $.function_call,
+        $.NULL,
+        $.TRUE,
+        $.FALSE,
       ),
     default_clause: $ =>
       seq(
@@ -1163,6 +1166,9 @@ module.exports = grammar({
       $.string,
       $._identifier,
       $.function_call,
+      $.NULL,
+      $.TRUE,
+      $.FALSE,
     ),
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
