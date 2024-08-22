c9bda18c5baa2ae66f523404c6d8ebd791d1cc43
Taishi Naka <taishi.develop@gmail.com>
2024-08-22 13:32:03 +0900

    Support arithmetic expressions in limit clause

--- grammar.js
+++ grammar.js
@@ -757,8 +757,7 @@ module.exports = grammar({
     limit_clause: $ =>
       seq(
         kw("LIMIT"),
-        choice($.number, kw("ALL")),
-        optional(seq(",", $.number)), // MySQL LIMIT a, b
+        choice(kw("ALL"), $.number, $.binary_expression, $.unary_expression)
       ),
     offset_clause: $ =>
       prec.right(
